11800052fac9409f01368341c1b208b50b95579b
Abhee <abhaynimbalkar03@gmail.com>
2024-11-06 22:19:32 +0530

    Optimised App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import NavBar from './Components/NavBar'
 import Home from './Pages/Home'
 import ProductContextProvider from './Context/Context'
-import Login from './Pages/Login'
-import Register from './Pages/Register'
-import Supplier from './Pages/Supplier'
+
 
 const App = () => {
 
